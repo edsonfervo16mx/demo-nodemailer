f6ad5b20d1eb0a3778e1d4fb15918597c2008c4b
edsonfervo16mx <edson55_@hotmail.com>
2020-11-20 21:21:23 -0600

    Pruebas base

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
+require("dotenv").config();
 const express = require("express")
 const app = express()
 
+const nodemailer = require("nodemailer")
+
+async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    //let testAccount = await nodemailer.createTestAccount();
+  
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: process.env.SERVIDOR,
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.EMAIL, // generated ethereal user
+        pass: process.env.PASSWORD, // generated ethereal password
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"no-reply-events " <no-reply-events@caribbeandigitalgroup.com>', // sender address
+      to: "j.gomez@caribbeandigitalgroup.com", // list of receivers
+      subject: "Hello with nodemailer", // Subject line
+      text: "Hello world", // plain text body
+      html: "<b>Hello world?, Test</b>", // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
+
 app.get("/",function(request,response){
     console.log("Start demo-nodemailer...")
+
+    main().catch(console.error);
 })
 
 app.listen(3000)
